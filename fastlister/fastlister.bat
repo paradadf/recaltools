394901c8e66d8e4c9b3d0034f21c1f62c3fcc61e
paradadf <paradadf@users.noreply.github.com>
2017-01-02 21:59:10 +0100

    Allow special characters on folder and file names!
    Fixes https://github.com/paradadf/recaltools/issues/6
    - Path and file names with special characters are now allowed.

--- fastlister/fastlister.bat
+++ fastlister/fastlister.bat
@@ -1,7 +1,6 @@
 @echo off
-setlocal EnableDelayedExpansion
 color 3f
-set releaseDate=16.12.2016
+set releaseDate=02.01.2017
 title fastlister ver. !releaseDate!
 
 rem List of excluded extensions
@@ -10,35 +9,35 @@ set excluded=.xml .txt
 :listFolders
 rem Show folder names in current directory
 echo Available folders for listing:
-for /d %%a in (*) do (
-set /a count+=1
-set mapArray[!count!]=%%a
-echo !count!: %%a
+set /a count=0
+for /f "delims=: tokens=1*" %%a in ('dir /b /a:d /o:n ^| findstr /n "."') do (
+	set "mapArray[%%a]=%%b"
+	set "count=%%a"
+	echo %%a: %%b
 )
 
 :checkFolders
+setlocal EnableDelayedExpansion
 if not exist !mapArray[1]! cls & echo No folders found on current directory^^! & goto SUB_folderBrowser
-if !count! gtr 1 set /a browser=!count!+1 & echo.!browser!: Open Folder Browser? & goto whichFolder
-if !count! equ 1 echo. & set /p "oneFolder=List the content of !mapArray[1]!? [Y/N]: "
-if /i "!oneFolder!"=="y" (set whichFolder=1 & goto validFolders) else goto SUB_folderBrowser
+if %count% gtr 0 set /a browser=%count%+1 & echo.!browser!: Open Folder Browser?
 
 :whichFolder
 echo.
 rem Select folder(s) to be listed
-echo The content of which folder(s) do you want to list? All? Choose !browser! to change directory.
+echo The content of which folder(s) do you want to list? All? Choose %browser% to change directory.
 set /p "whichFolder=#: "
-if !whichFolder! equ !browser! goto SUB_folderBrowser
+if "%whichFolder%"=="%browser%" goto SUB_folderBrowser
 if /i not "%whichFolder%"=="all" goto validFolders
 	set "folders="
-	for /f "delims=" %%f in ('dir /b /a:d') do set folders=!folders! "%%f"
+	for /l %%i in (1,1,%count%) do set folders=!folders! %%i
 	goto outputFolder
 
 :validFolders
 rem Create string containing all valid corresponding names
 set "folders="
 set /a count1=0
-for %%n  in (%whichFolder%) do (
-	if exist !mapArray[%%n]! (set folders=!folders! "!mapArray[%%n]!" & set /a count1+=1) else echo %%n is not a valid number^^!
+for %%i  in (%whichFolder%) do (
+	if exist !mapArray[%%i]! (set folders=!folders! %%i & set /a count1+=1) else echo %%i is not a valid number^^!
 )
 
 rem Check if input matched at least one folder
@@ -48,35 +47,38 @@ echo Your input did not match any folder, try again^^! & echo. & goto whichFolde
 :outputFolder
 rem Create output folder
 set destFolder=bestLists
-if not exist %~dp0%destFolder% md %~dp0%destFolder%
+if not exist "%~dp0%destFolder%" md "%~dp0%destFolder%"
 
 :listContent
 echo.
-title fastlister ver. !releaseDate! - Creating lists...
-for /d %%f in (%folders%) do (
-	cd %%~f && echo Scanning %%~f...
-	dir /b /a:-d /o:en 2>nul | findstr /v /i "%excluded%" >> "%~dp0%destFolder%\%%~f.txt"
+
+title fastlister ver. %releaseDate% - Creating lists...
+for %%f in (%folders%) do (
+	cd !mapArray[%%f]! && echo Scanning !mapArray[%%f]!...
+	dir /b /a:-d /o:en 2>nul | findstr /v /i "%excluded%" >> "%~dp0%destFolder%\!mapArray[%%f]!.txt"
 	cd ..
 )
 
 rem Detect and delete empty files on output folder
 echo.
+setlocal DisableDelayedExpansion
 for %%f in ("%~dp0%destFolder%\*") do (
 	if %%~zf equ 0 echo %%~nf has no elements to list. & del "%%f"
 )
 
 rem Delete output folder if empty
 echo.
-dir /a-d "%~dp0%destFolder%\*" >nul 2>nul && (echo Find the lists inside bestLists folder^^!) || (rd "%~dp0%destFolder%" & echo No lists created^^!)
+dir /a:-d "%~dp0%destFolder%\*" >nul 2>nul && (echo Find the lists inside bestLists folder!) || (rd "%~dp0%destFolder%" & echo No lists created!)
 
-title fastlister ver. !releaseDate! - Scanning directories finished^^!
+title fastlister ver. %releaseDate% - Scanning directories finished!
 
 popd & pause >nul & exit
 
 :SUB_folderBrowser
-if !count! lss 2 set /p "openBrowser=Open Folder Browser? [Y/N]: "
-if !count! lss 2 (
-	if not "!openBrowser!"=="y" exit
+setlocal DisableDelayedExpansion
+if %count% lss 2 set /p "openBrowser=Open Folder Browser? [Y/N]: "
+if %count% lss 2 (
+	if not "%openBrowser%"=="y" exit
 )
 set count=0 & echo Opening Folder Browser... & echo.
 
@@ -86,4 +88,4 @@ set "psCommand="(new-object -COM 'Shell.Application')^
 for /f "usebackq delims=" %%i in (`powershell %psCommand%`) do set "newRoot=%%i"
 
 rem Change working directory
-cls & pushd !newRoot! & goto listFolders
+cls & pushd %newRoot% & goto listFolders
