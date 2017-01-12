711695eb09b4e1c94f378473e799a07ffa17ae4b
paradadf <paradadf@users.noreply.github.com>
2017-01-12 17:26:07 +0100

    Cleanup the interface

--- fastunmount/fastunmount.bat
+++ fastunmount/fastunmount.bat
@@ -1,6 +1,6 @@
 @echo off
 color 3f
-set releaseDate=02.01.2017
+set releaseDate=12.01.2017
 title fastunmount ver. %releaseDate%
 
 rem List all mapped network drives
@@ -9,11 +9,29 @@ net use
 rem Disconnect selected network drives
 echo Which drive(s) do you want to unmount? All? (Ex. "z")
 set /p whichDrives=
-if /i "%whichDrives%"=="all" echo. & net use * /delete /yes & pause & goto:eof
+if /i not "%whichDrives%"=="all" echo. & goto selectedDrives
 
 echo.
-for %%a in (%whichDrives%) do (
-	if exist %%a:\ net use %%a: /delete
+
+for /F "tokens=1,2,3" %%a in ('net use^| find "\\"') do (
+  echo.Unmounting %%c from drive letter %%b
+  net use %%b /D
+)
+
+:selectedDrives
+rem Show warning for incorrect inputs
+for %%a in (%whichDrives:all=%) do (
+	if not exist %%a: echo %%a: not found!
+)
+echo.
+
+for /F "tokens=1,2,3" %%a in ('net use^| find "\\"') do (
+	for %%d in (%whichDrives%) do (
+		if /I "%%d:"=="%%b" (
+			echo.Unmounting %%c from drive letter %%b
+			net use %%b /D
+		)
+	)
 )
 
 title fastunmount ver. %releaseDate% - Unmounting finished!
